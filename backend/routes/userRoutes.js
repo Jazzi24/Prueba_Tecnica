c0533a993062b89a1a3a9b385ac9f1b3aee0e985
perezhernandezjazziel183@gmail.com <perezhernandezjazziel183@gmail.com>
2025-07-27 17:06:19 -0600

    Agregamos las rutas para los métodos

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,5 +5,8 @@ const auth = require('../middleware/auth');
 const upload = require('../middleware/upload');
 
 router.post('/', auth, upload.single('profilePicture'), userController.createUser);
+router.get('/', auth, userController.getUsers);
+router.put('/:id', auth, upload.single('profilePicture'), userController.updateUser);
+router.delete('/:id', auth, userController.deleteUser);
 
 module.exports = router;
